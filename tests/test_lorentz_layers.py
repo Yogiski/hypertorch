070c58b60fa3b9376378aba5edc06cfe2f3ba019
chris-yogodzinski <cyogodzi@gmail.com>
2025-11-02 13:01:30 -0500

    readme and tests outline

---
+++ tests/test_lorentz_layers.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestLorentzLayers(unittest.TestCase):
+
+    def test_lorentz_linear(self):
+        raise NotImplementedError
+
+    def test_lorentz_linear_parallel(self):
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    unittest.main()
