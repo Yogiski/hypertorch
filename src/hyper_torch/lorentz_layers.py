65c15285544df87645119fbd8b29c8cf21c1928c
chris-yogodzinski <cyogodzi@gmail.com>
2025-11-02 12:02:34 -0500

    batched lorentz linear

--- src/hyper_torch/lorentz_layers.py
+++ src/hyper_torch/lorentz_layers.py
@@ -24,3 +24,29 @@ class LorentzLinear(nn.Module):
         x_o_new = torch.sqrt(1.0 + torch.sum(x_s**2, dim=-1, keepdim=True))
 
         return torch.cat([x_o_new, x_s], dim=-1)
+
+
+class LorentzLinearParallel(nn.Module):
+
+    def __init__(self, n_proj, in_dim, out_dim, bias=True):
+
+        self.weights = nn.Parameter(
+            torch.randn(n_proj, out_dim - 1, in_dim - 1)
+        )
+        if bias:
+            self.bias = nn.Parameter(
+                torch.randn(n_proj, out_dim - 1)
+            )
+        else:
+            self.register_parameter("bias", None)
+
+    def forward(self, x: Tensor) -> Tensor:
+
+        x_s = x[..., 1:]
+        x_s = torch.einsum("...i,pji->...pj", x_s, self.weights)
+
+        if self.bias is not None:
+            x_s = x_s + self.bias
+
+        x_o_new = torch.sqrt(1.0 + torch.sum(x_s**2, dim=-1, keepdim=True))
+        return torch.cat([x_o_new, x_s], dim=-1)
